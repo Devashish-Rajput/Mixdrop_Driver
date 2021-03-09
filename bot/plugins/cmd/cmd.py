ddc81f0de9d3f3efb5d88a1f55b6cc545aa9bcf4
Devashish Rajput <42275894+Devashish-Rajput@users.noreply.github.com>
2021-03-09 21:51:38 +0530

    Update cmd.py

--- bot/plugins/cmd/cmd.py
+++ bot/plugins/cmd/cmd.py
@@ -1,8 +1,4 @@
 #!/usr/bin/env python3
-# This is bot coded by Abhijith-cloud and used for educational purposes only
-# https://github.com/Abhijith-cloud
-# Copyright ABHIJITH N T
-# Thank you https://github.com/pyrogram/pyrogram
 
 from bot.filetocloud import CloudBot, filters
 from bot import Msg
